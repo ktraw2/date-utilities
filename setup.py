359bbf88a552232078315496d9faf6bdedf75dc7
ktraw <ktraw2@gmail.com>
2019-02-15 02:32:05 -0800

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 from distutils.core import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="date_utilities",
     version="1.0",
     description="Date subtraction, parsing, and random timestamp generation.",
     author="Kevin Traw",
     author_email="ktraw2@gmail.com",
-    url="https://github.com/ktraw2/date_utilities",
-    packages=[".", ".tests"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ktraw2/date-utilities",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPL-3.0",
+        "Operating System :: OS Independent",
+    ],
     requires=["pytz"],
 )
