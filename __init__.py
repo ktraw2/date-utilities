06505f7f56a5ee93bc5a9b4d0ed049ec210416f2
Kevin <ktraw2@users.noreply.github.com>
2019-02-16 01:37:04 -0800

    Delete __init__.py

--- __init__.py
+++
@@ -1,4 +0,0 @@
-"""
-This package contains functions to subtract dates from each other in string and datetime format, generate random
-timestamps in a given time range, and parse date strings of a specific format.
-"""
